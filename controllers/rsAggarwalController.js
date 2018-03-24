5250866575b55421185e04b9223ef1fb352a1f0a
Gaurav Kumar <gaurav.kr.9637@gmail.com>
2018-03-24 11:23:45 +0530

    Changes in Text

--- controllers/rsAggarwalController.js
+++ controllers/rsAggarwalController.js
@@ -1,4 +1,4 @@
-const chemistryTableInitialStr = '<table class="topicListBox innerPage"><tbody><tr><td class="headingBox" style="text-align: center; padding: 10px;" colspan="2"><b><a href="https://byjus.com/chemistry/chemistry-article/">More Chemistry Articles</a></b></td></tr>';
+const chemistryTableInitialStr = '<table class="topicListBox innerPage"><tbody><tr><td class="headingBox" style="text-align: center; padding: 10px;" colspan="2"><b>More RS Aggarwal Solutions</b></td></tr>';
 const chemistryTableFinalStr = '</tbody></table>';
 const chemistryTabledummyStr = '<td class="contentBox" style="text-align: center;"><a href="%link%">%title%</a></td>';
 
